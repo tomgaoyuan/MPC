e74fc65ba03012b7e77d45af3759c9bf5d069481
tomgaoyuan <tomgaoyuan@yahoo.com>
2015-12-02 22:44:59 +0800

    Update RFPMmain.m

--- RFPMmain.m
+++ RFPMmain.m
@@ -19,8 +19,8 @@ SYSTEM.X = [ -1 1];
 SYSTEM.Y = [ -1 1];
 
 %FIRST PHASE
-discreteX = linspace( SYSTEM.X(1), SYSTEM.X(2), 1000);
-discreteY = linspace( SYSTEM.Y(1), SYSTEM.Y(2), 1000); 
+discreteX = linspace( SYSTEM.X(1), SYSTEM.X(2), 100);
+discreteY = linspace( SYSTEM.Y(1), SYSTEM.Y(2), 100); 
 %establish the dataBase of average 
 flag = zeros( length(discreteX) , length(discreteY) );    %0 for not conclude, 1 for correct, -1 for no enough data 
 fingerPrint = zeros( length(discreteX) , length(discreteY), PATH);
@@ -29,27 +29,38 @@ for i = 1: length(discreteX)
         X = [ discreteX(i) discreteY(j) ];
         try 
             re = MPCMaker(X, S, PATH);
+        catch ME
+            flag(i, j) =-1;
+            break;
+        end
             d = sqrt( sum( ( re - repmat( X, size( re, 1), 1) ).^2, 2) );
             sort(d);
             tdoa = ( d(2:end) - d(1) ) / c;
             fingerPrint( i, j, :) = tdoa;
             flag(i, j) =1;
-        catch ME
-            flag(i, j) =-1;
-        end
     end
 end
 
 X = rand(1,2) .* [SYSTEM.X(2) - SYSTEM.X(1), SYSTEM.Y(2) - SYSTEM.Y(1)] + [  SYSTEM.X(1) SYSTEM.Y(1) ];
-tdoaSample = fingerPrint( 2, 2, :);
-distance = Inf;
-re = [];
-for i = 1: length(discreteX)
-    for j = 1:length(discreteY)
-        tmp = sqrt( sum( (fingerPrint(i, j, :) - tdoaSample).^2) );
-        if  tmp < distance
-            distance = tmp;
-            re = [i j];
-        end
-    end
-end
+re = MPCMaker(X, S, PATH);
+d = sqrt( sum( ( re - repmat( X, size( re, 1), 1) ).^2, 2) );
+sort(d);
+tdoa = ( d(2:end) - d(1) ) / c;
+
+fingerPrintCol = reshape( fingerPrint, size(fingerPrint,1) * size(fingerPrint,2), PATH );
+idx = knnsearch( fingerPrintCol, tdoa.');
+idx = [ mod(idx, size(fingerPrint,1)) fix(idx/size(fingerPrint,1)) ];
+X
+[ discreteX( idx(1) ) discreteY( idx(2) ) ]
+
+% distance = Inf;
+% re = [];
+% for i = 1: length(discreteX)
+%     for j = 1:length(discreteY)
+%         tmp = sqrt( sum( (fingerPrint(i, j, :) - tdoaSample).^2) );
+%         if  tmp < distance
+%             distance = tmp;
+%             re = [i j];
+%         end
+%     end
+% end
